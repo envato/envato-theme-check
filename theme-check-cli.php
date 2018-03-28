e3e36b974bb2bec1f7a2119b4e1b2323153446af
Aaron Rutley <aaron.rutley@gmail.com>
2018-03-28 15:56:26 +1100

    fix more typos

--- theme-check-cli.php
+++ theme-check-cli.php
@@ -55,7 +55,7 @@ class ThemeCheckCLI extends WP_CLI_Command {
 		// empty array for the json format
 		$required_json = array();
 		$warnings_json = array();
-		$reccomended_json = array();
+		$recommended_json = array();
 		$errors_json = array();
 		$result_json = array();
 
@@ -108,7 +108,7 @@ class ThemeCheckCLI extends WP_CLI_Command {
 
 			} elseif ( 'RECOMMENDED' == trim( $type ) ) {
 				if( 'true' == $assoc_args['format'] ) {
-					array_push($reccomended_json, "Recommended: ".trim( $message ));
+					array_push($recommended_json, "Recommended: ".trim( $message ));
 				} else {
 					WP_CLI::warning( '%cRecommended:%n '.trim( $message ) );
 				}
