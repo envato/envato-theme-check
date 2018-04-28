a963198d03a5038ae76b02cccdb0da7840aea559
Scott Parry <hello@ikreativ.com>
2018-04-28 08:18:35 +0100

    Improved balance_tags checks to create less false positives

--- checks/themeforest.php
+++ checks/themeforest.php
@@ -36,15 +36,17 @@ class Themeforest implements themecheck {
 		);
 
 		$warn_checks = array(
-			'/@\$/'                                     => __( 'Possible error suppression is being used', 'theme-check' ),
-			'/@include/'                                => __( 'Possible error suppression is being used', 'theme-check' ),
-			'/@require/'                                => __( 'Possible error suppression is being used', 'theme-check' ),
-			'/@file/'                                   => __( 'Possible error suppression is being used', 'theme-check' ),
-			'/[^a-z0-9](?<!_)balanceTags\s?\(/'         => __( 'Possible data validation issues found. balanceTags() does not escape data', 'theme-check' ),
-			'/[^a-z0-9](?<!_)force_balance_tags\s?\(/'  => __( 'Possible data validation issues found. force_balance_tags() does not escape data', 'theme-check' ),
-			'/echo \$/'                                 => __( 'Possible data validation issues found. All dynamic data must be correctly escaped for the context where it is rendered', 'theme-check' ),
-			'/[^a-z0-9](?<!_)\$_SERVER\s?/'             => __( 'PHP Global Variable found. Ensure the context is safe and reliable', 'theme-check' ),
-			'/remove_filter\s?\(/'                      => __( 'Themes should not remove core filters. Ensure this is a valid use case', 'theme-check' ),
+			'/@\$/'                                        => __( 'Possible error suppression is being used', 'theme-check' ),
+			'/@include/'                                   => __( 'Possible error suppression is being used', 'theme-check' ),
+			'/@require/'                                   => __( 'Possible error suppression is being used', 'theme-check' ),
+			'/@file/'                                      => __( 'Possible error suppression is being used', 'theme-check' ),
+			'/[^a-z0-9](?<!_)balanceTags\s?\(\$/'          => __( 'Possible data validation issues found. balanceTags() does not escape data', 'theme-check' ),
+			'/[^a-z0-9](?<!_)balanceTags\s?\( \$/'         => __( 'Possible data validation issues found. balanceTags() does not escape data', 'theme-check' ),
+			'/[^a-z0-9](?<!_)force_balance_tags\s?\(\$/'   => __( 'Possible data validation issues found. force_balance_tags() does not escape data', 'theme-check' ),
+			'/[^a-z0-9](?<!_)force_balance_tags\s?\( \$/'  => __( 'Possible data validation issues found. force_balance_tags() does not escape data', 'theme-check' ),
+			'/echo \$/'                                    => __( 'Possible data validation issues found. All dynamic data must be correctly escaped for the context where it is rendered', 'theme-check' ),
+			'/[^a-z0-9](?<!_)\$_SERVER\s?/'                => __( 'PHP Global Variable found. Ensure the context is safe and reliable', 'theme-check' ),
+			'/remove_filter\s?\(/'                         => __( 'Themes should not remove core filters. Ensure this is a valid use case', 'theme-check' ),
 		);
 
 		$grep = '';
