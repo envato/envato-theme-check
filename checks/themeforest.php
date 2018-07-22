e4c0e65b7a08ece1e46a0119e8ddf9f5495342b8
Scott Parry <hello@ikreativ.com>
2018-07-22 09:25:59 +0100

    Check for unescaped translations.

--- checks/themeforest.php
+++ checks/themeforest.php
@@ -40,6 +40,7 @@ class Themeforest implements themecheck {
 			'/is_plugin_active\s?\(/'                       => __( 'is_plugin_active() is not reliable. Use function_exists() or class_exists() instead', 'theme-check' ),
 			'/add_action\( &\$this/'                        => __( 'When creating a callable, never use &$this, use $this instead', 'theme-check' ),
 			'/admin_bar_menu/'                              => __( 'Themes must not add any entries to the admin bar', 'theme-check' ),
+			'/(^|\s)(__|_e|_n|_x|_nx|_ex)\s*?\(/'           => __( 'All translatable strings must be escaped to prevent malicious code injection from a translation', 'theme-check' ),
 		);
 
 		$warn_checks = array(
