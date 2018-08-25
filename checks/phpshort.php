066a2b5b5efd9c53f90c40465bc369b547a25cdd
Scott Parry <hello@ikreativ.com>
2018-08-25 17:16:08 +0100

    PHP shorttags false positives

--- checks/phpshort.php
+++ checks/phpshort.php
@@ -8,9 +8,9 @@ class PHPShortTagsCheck implements themecheck {
 
 		foreach ( $php_files as $php_key => $phpfile ) {
 			checkcount();
-			if ( preg_match( '/<\?(\=?)(?!php|xml)/i', $phpfile ) ) {
+			if ( preg_match( '/<\?\=/i', $phpfile ) ) {
 				$filename = tc_filename( $php_key );
-				$grep = tc_preg( '/<\?(\=?)(?!php|xml)/', $php_key );
+				$grep = tc_preg( '/<\?\=/', $php_key );
 				$this->error[] = sprintf('<span class="tc-lead tc-warning">'.__('WARNING','theme-check').'</span>: '.__('Found PHP short tags in file %1$s.%2$s', 'theme-check'), '<strong>' . $filename . '</strong>', $grep);
 				$ret = false;
 			}
